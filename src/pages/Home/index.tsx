6c463930cc9a22d9c258156e968171f5e7e95a70
JokoSetiawan-dev <jokosetiawan.career@gmail.com>
2023-09-27 07:25:57 +0700

    fix: get data category

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,70 +1,62 @@
-import React, { useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./home.module.css";
 
 interface IDataItem {
   id: string;
   name: string;
-  isActive: string;
+  isActive: boolean;
 }
 
 interface DataTableProps {
   data: IDataItem[];
 }
 
-
 const HomePage: React.FC<DataTableProps> = () => {
-    const [data, setData] = useState<IDataItem[]>([]);
+  const [data, setData] = useState<IDataItem[]>([]);
 
-    const fetchData = async () => {
-        const apiUrl = 'https://mock-api.arikmpt.com/api/category?page=1&name=mock category'
-        try {
-            const response = await fetch(apiUrl, {
-                headers: {
-                    Authorization: `Bearer ${localStorage.getItem('authToken')}`
-                }
-            })
-            const data = await response.json()
-            setData(data.data)
-            console.log(data.current_page);
-            
-        } catch (error) {
-            console.log(error);
-        }
+  const fetchData = async () => {
+    const apiUrl = "https://mock-api.arikmpt.com/api/category";
+    try {
+      const response = await fetch(apiUrl, {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("authToken")}`,
+        },
+      });
+      const data = await response.json();
+      setData(data.data);
+      console.log(data.data);
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    useEffect(() => {
-        fetchData()
-    }, []);
-    
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   return (
-    
     <div className={`container pt-4 ${styles.homeContainer}`}>
-
       <table className="table">
-      <thead>
-        <tr>
-          <th scope="col">No</th>
-          <th scope="col">id</th>
-          <th scope="col">Name</th>
-          <th scope="col">Status</th>
-          <th scope="col">Action</th>
-        </tr>
-      </thead>
-      <tbody>
-        {data.map((data) => (
-          <tr key={data.id}>
-            <th scope="row">1</th>
-            <td>{data.id}</td>
-            <td>{data.name}</td>
-            <td>{data.isActive}</td>
-            <td>
-
-            </td>
+        <thead>
+          <tr>
+            <th scope="col">Id</th>
+            <th scope="col">Name</th>
+            <th scope="col">Status</th>
+            <th scope="col">Action</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {data.map((data) => (
+            <tr key={data.id}>
+              <td>{data.id}</td>
+              <td>{data.name}</td>
+              <td>{data.isActive ? "Active" : "Deactive"}</td>
+              <td></td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
